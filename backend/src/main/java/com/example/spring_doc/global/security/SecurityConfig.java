226b55173a404d870623b27d32a82046269f89cb
chacha86 <taejincha0619@gmail.com>
2025-02-12 11:10:37 +0900

    swagger dependency & permit

--- backend/src/main/java/com/example/spring_doc/global/security/SecurityConfig.java
+++ backend/src/main/java/com/example/spring_doc/global/security/SecurityConfig.java
@@ -32,6 +32,8 @@ public class SecurityConfig {
                                 .permitAll()
                                 .requestMatchers("/api/v1/posts/statistics")
                                 .hasRole("ADMIN")
+                                .requestMatchers("/swagger-ui/**")
+                                .permitAll()
                                 .anyRequest()
                                 .authenticated()
                 )
